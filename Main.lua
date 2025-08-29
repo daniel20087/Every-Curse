025ce9ede20ccbd2f0db782a8556bf0d28c20f27
daniel8757 <69530004+daniel20087@users.noreply.github.com>
2025-08-29 05:46:42 +0000

    Add files via upload
    added a warning to enter via mega satan on chest floor to reach void

--- Main.lua
+++ Main.lua
@@ -1,28 +1,21 @@
-local EveryCurseEveryFloor = RegisterMod("Every Curse", 1)
+local XLCurseEveryFloor = RegisterMod("Every Floor Is XL", 1)
 
-function EveryCurseEveryFloor:GiveEveryCurse()
-    
+function XLCurseEveryFloor:GiveXLCurse()
+
     if Game().Challenge ~= Challenge.CHALLENGE_NULL or Game():IsGreedMode() then -- no greed or challenge
         return
     end
-
-    -- Get current level.
-    local level = Game():GetLevel()
     local isaacPlayerID = Isaac.GetPlayer()
-    -- no void, hush, womb ii, mom
-    local stageCurse = level:GetAbsoluteStage()
-    if stageCurse == LevelStage.STAGE7 or stageCurse == LevelStage.STAGE3_2 or stageCurse == LevelStage.STAGE4_2 or stageCurse == LevelStage.STAGE4_3 or stageCurse == LevelStage.STAGE4_3 then
-        return (
-        LevelCurse.CURSE_OF_BLIND |
-        LevelCurse.CURSE_OF_MAZE |
-        LevelCurse.CURSE_OF_THE_LOST |
-        LevelCurse.CURSE_OF_DARKNESS|
-        LevelCurse.CURSE_OF_THE_UNKNOWN
-        )
+    -- get current floor.
+    local level = Game():GetLevel()
+    local stageXL = level:GetAbsoluteStage()
+    --below code stops void, mom, womb ii, and hush from having xl
+    if stageXL == LevelStage.STAGE7 or stageXL == LevelStage.STAGE3_2 or stageXL == LevelStage.STAGE4_2 or stageXL == LevelStage.STAGE4_3 then
+        return
     end
     local hasKeyPiece1 = isaacPlayerID:GetCollectibleNum(CollectibleType.COLLECTIBLE_KEY_PIECE_1)
     local hasKeyPiece2 = isaacPlayerID:GetCollectibleNum(CollectibleType.COLLECTIBLE_KEY_PIECE_2)
-    if stageCurse == LevelStage.STAGE6 then -- this is for the player to be able to reach void in a run as void portal doesnt spawn in blue baby fight
+    if stageXL == LevelStage.STAGE6 then -- this is for the player to be able to reach void in a run as void portal doesnt spawn in blue baby fight so they enter via mega satan fight
         if hasKeyPiece1 == 0 then
             isaacPlayerID:AddCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_1)
         end
@@ -31,26 +24,15 @@ function EveryCurseEveryFloor:GiveEveryCurse()
             isaacPlayerID:AddCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_2)
         end
 
-        return(
-        LevelCurse.CURSE_OF_LABYRINTH |
-        LevelCurse.CURSE_OF_BLIND |
-        LevelCurse.CURSE_OF_MAZE |
-        LevelCurse.CURSE_OF_THE_LOST |
-        LevelCurse.CURSE_OF_DARKNESS|
-        LevelCurse.CURSE_OF_THE_UNKNOWN
-    )
+        return
+            LevelCurse.CURSE_OF_LABYRINTH
     end
-    return (
-        LevelCurse.CURSE_OF_LABYRINTH |
-        LevelCurse.CURSE_OF_BLIND |
-        LevelCurse.CURSE_OF_MAZE |
-        LevelCurse.CURSE_OF_THE_LOST |
-        LevelCurse.CURSE_OF_DARKNESS|
-        LevelCurse.CURSE_OF_THE_UNKNOWN
-    )
+
+    return LevelCurse.CURSE_OF_LABYRINTH
 end
 
-function EveryCurseEveryFloor:OnPickupMorph(pickup)
+--change all black candle spawns to random item
+function XLCurseEveryFloor:OnPickupMorph(pickup)
     if pickup.Variant ~= PickupVariant.PICKUP_COLLECTIBLE then return end
     if pickup.SubType ~= CollectibleType.COLLECTIBLE_BLACK_CANDLE then return end
 
@@ -59,7 +41,7 @@ function EveryCurseEveryFloor:OnPickupMorph(pickup)
     local itemConfig = Isaac.GetItemConfig()
 
     while not isPassive do
-        randomItemID = math.random(1, 719) --if anyone wants to maintain this code in the future go right ahead
+        randomItemID = math.random(1, 732) --if anyone wants to maintain this code in the future go right ahead
         local collectibleConfig = itemConfig:GetCollectible(randomItemID)
         if collectibleConfig and collectibleConfig.Type == ItemType.ITEM_PASSIVE and randomItemID ~= CollectibleType.COLLECTIBLE_BLACK_CANDLE then
             isPassive = true
@@ -68,6 +50,14 @@ function EveryCurseEveryFloor:OnPickupMorph(pickup)
 
     pickup:Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, randomItemID, true, true, true)
 end
+function XLCurseEveryFloor:MegaSatanWarning()
+    local isaacHud = Game():GetHUD()
+    if Game():GetLevel():GetAbsoluteStage() == LevelStage.STAGE6 then
 
-EveryCurseEveryFloor:AddCallback(ModCallbacks.MC_POST_CURSE_EVAL, EveryCurseEveryFloor.GiveEveryCurse)
-EveryCurseEveryFloor:AddCallback(ModCallbacks.MC_POST_PICKUP_INIT, EveryCurseEveryFloor.OnPickupMorph)
+        isaacHud:ShowFortuneText("Go through mega satan to reach void")
+        
+    end
+end
+XLCurseEveryFloor:AddCallback(ModCallbacks.MC_POST_CURSE_EVAL, XLCurseEveryFloor.GiveXLCurse)
+XLCurseEveryFloor:AddCallback(ModCallbacks.MC_POST_PICKUP_INIT, XLCurseEveryFloor.OnPickupMorph)
+XLCurseEveryFloor:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, XLCurseEveryFloor.MegaSatanWarning)
